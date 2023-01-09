2d05b3b5d84312df631acd944b75322970f32c67
sadiq <sadiqhus@cisco.com>
2023-01-09 10:08:02 +0530

    Revert "Added stack implementation using linked list"
    This reverts commit 86ac5795cd27851fdb253510d7713c22b64b2cdb.

--- src/com/allaboutjava/basicconcepts/datastructure/list/custom/SingleLinkedListNode.java
+++ src/com/allaboutjava/basicconcepts/datastructure/list/custom/SingleLinkedListNode.java
@@ -1,28 +1,13 @@
 package com.allaboutjava.basicconcepts.datastructure.list.custom;
 
 public class SingleLinkedListNode {
-
-  Object data;
+  int data;
   SingleLinkedListNode next;
+  int size;
 
-  public SingleLinkedListNode(Object data) {
+  public SingleLinkedListNode(int data) {
     this.data = data;
     next = null;
   }
 
-  public SingleLinkedListNode getNext() {
-    return next;
-  }
-
-  public void setNext(SingleLinkedListNode next) {
-    this.next = next;
-  }
-
-  public Object getData() {
-    return data;
-  }
-
-  public void setData(Object data) {
-    this.data = data;
-  }
 }
