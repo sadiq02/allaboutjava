86ac5795cd27851fdb253510d7713c22b64b2cdb
sadiq <sadiqhus@cisco.com>
2023-01-09 10:05:40 +0530

    Added stack implementation using linked list

--- src/com/allaboutjava/basicconcepts/datastructure/list/custom/SingleLinkedListNode.java
+++ src/com/allaboutjava/basicconcepts/datastructure/list/custom/SingleLinkedListNode.java
@@ -1,13 +1,28 @@
 package com.allaboutjava.basicconcepts.datastructure.list.custom;
 
 public class SingleLinkedListNode {
-  int data;
-  SingleLinkedListNode next;
-  int size;
 
-  public SingleLinkedListNode(int data) {
+  Object data;
+  SingleLinkedListNode next;
+
+  public SingleLinkedListNode(Object data) {
     this.data = data;
     next = null;
   }
 
+  public SingleLinkedListNode getNext() {
+    return next;
+  }
+
+  public void setNext(SingleLinkedListNode next) {
+    this.next = next;
+  }
+
+  public Object getData() {
+    return data;
+  }
+
+  public void setData(Object data) {
+    this.data = data;
+  }
 }
