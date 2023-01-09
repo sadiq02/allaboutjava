adf8ce5bc5297228eb0892c3497a26fb64179399
sadiq <sadiqhus@cisco.com>
2023-01-09 10:05:40 +0530

    Added stack implementation using linked list
    (cherry picked from commit 86ac5795cd27851fdb253510d7713c22b64b2cdb)

---
+++ src/com/allaboutjava/basicconcepts/datastructure/stack/StackUsingLinkedList.java
@@ -0,0 +1,78 @@
+package com.allaboutjava.basicconcepts.datastructure.stack;
+
+import com.allaboutjava.basicconcepts.datastructure.list.custom.SingleLinkedListNode;
+
+public class StackUsingLinkedList {
+
+  int size;
+  SingleLinkedListNode head;
+
+  StackUsingLinkedList() {
+    this.size = 0;
+    head = null;
+  }
+
+  public boolean isEmpty() {
+    return size == 0;
+  }
+
+  public int getCurrentSize() {
+    return size;
+  }
+
+  public void push(Object data) {
+    SingleLinkedListNode newNode = new SingleLinkedListNode(data);
+    if (isEmpty()) {
+      head = newNode;
+      size++;
+      return;
+    }
+    newNode.setNext(head);
+    head = newNode;
+    size++;
+  }
+
+  public void display() {
+    System.out.println("Stack content\n");
+    SingleLinkedListNode temp = head;
+    for (int i = 0; i < size; i++) {
+      System.out.println(temp.getData());
+      temp = temp.getNext();
+    }
+  }
+
+  public void pop() {
+    if (isEmpty()) {
+      System.out.println("Stack is empty, nothing to remove!");
+      return;
+    }
+    System.out.println("Item popped : " + head.getData());
+    head = head.getNext();
+    size--;
+  }
+
+  public Object peek() {
+    if (isEmpty()) {
+      System.out.println("Stack is empty, nothing to peek!");
+      return null;
+    }
+    return head.getData();
+  }
+
+  public static void main(String[] args) {
+    StackUsingLinkedList myStack = new StackUsingLinkedList();
+    System.out.println("IsEmpty? : " + myStack.isEmpty());
+    myStack.push(1);
+    myStack.push(2);
+    myStack.push(3);
+    myStack.push(55);
+    System.out.println("Current size : " + myStack.size);
+    myStack.display();
+    System.out.println("Latest added item : " + myStack.peek());
+    myStack.pop();
+    myStack.pop();
+    myStack.display();
+    System.out.println("Current size - " + myStack.size);
+  }
+
+}
