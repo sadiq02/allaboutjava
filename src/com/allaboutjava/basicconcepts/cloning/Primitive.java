f69b96a4f446988b9d9af2442c2f718c8ef2da0f
sadiq <sadiqhus@cisco.com>
2023-02-02 09:52:06 +0530

    Added a program for primitive data type cloning

---
+++ src/com/allaboutjava/basicconcepts/cloning/Primitive.java
@@ -0,0 +1,64 @@
+package com.allaboutjava.basicconcepts.cloning;
+
+public class Primitive {
+
+  static class Person implements Cloneable {
+
+    int age;
+    String name;
+    String country;
+
+    public Person(int age, String name, String country) {
+      this.age = age;
+      this.name = name;
+      this.country = country;
+    }
+
+    public int getAge() {
+      return age;
+    }
+
+    public void setAge(int age) {
+      this.age = age;
+    }
+
+    public String getName() {
+      return name;
+    }
+
+    public void setName(String name) {
+      this.name = name;
+    }
+
+    public String getCountry() {
+      return country;
+    }
+
+    public void setCountry(String country) {
+      this.country = country;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+      return super.clone();
+    }
+
+    @Override
+    public String toString() {
+      return "Person{" +
+          "age=" + age +
+          ", name='" + name + '\'' +
+          ", country='" + country + '\'' +
+          '}';
+    }
+  }
+
+  public static void main(String[] args) throws CloneNotSupportedException {
+    Person person = new Person(33, "Sadiq", "India");
+    Person clonedPerson = (Person) person.clone();
+    clonedPerson.setAge(34);
+    clonedPerson.setName("Sarine");
+    System.out.println("Original object - " + person.toString());
+    System.out.println("Cloned object - " + clonedPerson.toString());
+  }
+}
