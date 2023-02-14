818436f9cdf3ffbdb7d9c82babf6d457532a3dde
sadiq <sadiqhus@cisco.com>
2023-02-14 17:24:15 +0530

    Added program to demo jdk11 readString & writeString methods

--- src/com/allaboutjava/basicconcepts/misc/StringInJdk11.java
+++ src/com/allaboutjava/basicconcepts/misc/StringInJdk11.java
@@ -1,16 +1,27 @@
 package com.allaboutjava.basicconcepts.misc;
 
-import java.util.stream.Collectors;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
 public class StringInJdk11 {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     String s = "Sadiq Hussain";
     System.out.println(s.repeat(4));
     System.out.println(s.strip());
 
     String s1 = "Sadiq\nSarine\nDanish\nAmara";
     System.out.println(s1.lines().count());
+
+    Path filePath = Path.of("/Users/sadiqhus/mygit/personal/allaboutjava/sample_files/test.txt");
+    String fileContent = Files.readString(filePath);
+    System.out.println(fileContent);
+
+    Path filePath1 = Path.of("/Users/sadiqhus/mygit/personal/allaboutjava/sample_files/test2.txt");
+    Files.writeString(filePath1, fileContent);
+   /* String fileContent1 = Files.readString(filePath1);
+    System.out.println(fileContent1);*/
   }
 
 }
