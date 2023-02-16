8a15e5cd93159f23aa2578338b0b5409bbab48ce
sadiq <sadiqhus@cisco.com>
2023-02-16 15:24:50 +0530

    Added practice program

---
+++ src/com/allaboutjava/practice/MaxNumberInArray.java
@@ -0,0 +1,16 @@
+package com.allaboutjava.practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MaxNumberInArray {
+
+  public static void main(String[] args) {
+    List<Integer> input = new ArrayList<>();
+    input.add(10);
+    input.add(20);
+    input.add(30);
+    input.add(40);
+    System.out.println(input.stream().mapToInt(x->x).summaryStatistics().getMax());
+  }
+}
