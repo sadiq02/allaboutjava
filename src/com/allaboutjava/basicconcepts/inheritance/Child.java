7c66058e9d08dfa0a6ebd9925b518780d3f7aab8
Sadiq Hussain <sadiqhussain96@gmail.com>
2023-02-27 18:03:12 +0530

    Added sample program for inheritance

---
+++ src/com/allaboutjava/basicconcepts/inheritance/Child.java
@@ -0,0 +1,10 @@
+package com.allaboutjava.basicconcepts.inheritance;
+
+public class Child extends Parent{
+
+  int x = 20;
+
+  void show() {
+    System.out.println("Inside Child.show()");
+  }
+}
