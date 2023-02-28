dbe204fd484db5a7ec45e15d36c63ad77491d495
Sadiq Hussain <sadiqhussain96@gmail.com>
2023-02-28 15:32:37 +0530

    Added program to counter characters in a string

---
+++ src/com/allaboutjava/basicconcepts/problemsolving/CountCharactersWithoutLoop.java
@@ -0,0 +1,23 @@
+package com.allaboutjava.basicconcepts.problemsolving;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountCharactersWithoutLoop {
+
+  public static void main(String[] args) {
+    Map<Character, Integer> map = new HashMap<>();
+    String input = "this is alksjdjkvaksejuwqerylaksjfakjsdfkasdhfkljshdfkjsahdf";
+    for (int i = 0; i < input.length(); i++) {
+      if (map.containsKey(input.charAt(i))) {
+        map.put(input.charAt(i), map.get(input.charAt(i)) + 1);
+      } else {
+        map.put(input.charAt(i), 1);
+      }
+    }
+    for (Map.Entry<Character, Integer> temp : map.entrySet()) {
+      System.out.println(temp.getKey() + " - " + temp.getValue());
+    }
+
+  }
+}
